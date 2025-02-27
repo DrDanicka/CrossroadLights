3ac61ef325be16fab72939d99317df798292a446
Daniel Šoltýs <danielsytlos@gmail.com>
2025-02-27 23:23:09 +0100

    Added crossroad validator

---
+++ CrossRoadConfig/crossroad_validator.py
@@ -0,0 +1,37 @@
+from orientation import Orientation
+
+def validate_crossroad(roads):
+    road_orientations = list(roads.keys())
+    straight_mapping = {
+        Orientation.NORTH: Orientation.SOUTH,
+        Orientation.EAST: Orientation.WEST,
+        Orientation.SOUTH: Orientation.NORTH,
+        Orientation.WEST: Orientation.EAST
+    }
+
+    left_mapping = {
+        Orientation.NORTH: Orientation.WEST,
+        Orientation.EAST: Orientation.NORTH,
+        Orientation.SOUTH: Orientation.EAST,
+        Orientation.WEST: Orientation.SOUTH
+    }
+
+    right_mapping = {
+        Orientation.NORTH: Orientation.EAST,
+        Orientation.EAST: Orientation.SOUTH,
+        Orientation.SOUTH: Orientation.WEST,
+        Orientation.WEST: Orientation.NORTH
+    }
+
+    for orientation, road in roads.items():
+        if road.directions["Straight"]:
+            if straight_mapping[orientation] not in road_orientations:
+                return (False, f"Road from {orientation.name[0]} does not have a road going straight.")
+        if road.directions["Left"]:
+            if left_mapping[orientation] not in road_orientations:
+                return (False, f"Road from {orientation.name[0]} does not have a road going left.")
+        if road.directions["Right"]:
+            if right_mapping[orientation] not in road_orientations:
+                return (False, f"Road from {orientation.name[0]} does not have a road going right.")
+
+    return (True, '')
