34b32a8f227709a9049d390057550625ff89d46c
Daniel Soltys <danielsytlos@gmail.com>
2025-02-27 12:48:54 +0100

    Added class structure for roads

--- CrossRoadConfig/main.py
+++ CrossRoadConfig/main.py
@@ -2,8 +2,7 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 import json
 from enum import Enum
-from drawing_roads import draw_road_north, draw_road_west, draw_road_south, draw_road_east
-
+from road import RoadNorth, RoadEast, RoadSouth, RoadWest
 
 class Orientation(Enum):
     NORTH = 1
@@ -24,28 +23,29 @@ class TrafficLightConfigurator:
         self.canvas.create_text(250, 480, text="S", font=("Arial", 24, "bold"))
         self.canvas.create_text(20, 250, text="E", font=("Arial", 24, "bold"))
 
-        self.config = {"roads": []}
+        self.roads = []
+        self.buttons = {}
 
         self.btn_frame = tk.Frame(root)
         self.btn_frame.pack()
 
-        self.btn_add_road_n = tk.Button(self.btn_frame, text="Add Road N", command=lambda: self.add_road(Orientation.NORTH))
-        self.btn_add_road_n.pack(side="left", padx=5)
+        self.buttons[Orientation.NORTH] = tk.Button(self.btn_frame, text="Add Road N", command=lambda: self.add_road(Orientation.NORTH))
+        self.buttons[Orientation.NORTH].pack(side="left", padx=5)
 
-        self.btn_add_road_s = tk.Button(self.btn_frame, text="Add Road S", command=lambda: self.add_road(Orientation.SOUTH))
-        self.btn_add_road_s.pack(side="left", padx=5)
+        self.buttons[Orientation.SOUTH] = tk.Button(self.btn_frame, text="Add Road S", command=lambda: self.add_road(Orientation.SOUTH))
+        self.buttons[Orientation.SOUTH].pack(side="left", padx=5)
 
-        self.btn_add_road_e = tk.Button(self.btn_frame, text="Add Road E", command=lambda: self.add_road(Orientation.EAST))
-        self.btn_add_road_e.pack(side="left", padx=5)
+        self.buttons[Orientation.EAST] = tk.Button(self.btn_frame, text="Add Road E", command=lambda: self.add_road(Orientation.EAST))
+        self.buttons[Orientation.EAST].pack(side="left", padx=5)
 
-        self.btn_add_road_w = tk.Button(self.btn_frame, text="Add Road W", command=lambda: self.add_road(Orientation.WEST))
-        self.btn_add_road_w.pack(side="left", padx=5)
+        self.buttons[Orientation.WEST] = tk.Button(self.btn_frame, text="Add Road W", command=lambda: self.add_road(Orientation.WEST))
+        self.buttons[Orientation.WEST].pack(side="left", padx=5)
 
         self.btn_save = tk.Button(root, text="Save Config", command=self.save_config)
         self.btn_save.pack()
 
     def add_road(self, orientation: Orientation):
-        if len(self.config["roads"]) >= 4:
+        if len(self.roads) >= 4:
             messagebox.showwarning("Limit Reached", "A crossroad can have a maximum of 4 roads.")
             return
 
@@ -53,7 +53,7 @@ class TrafficLightConfigurator:
         road_window.title("Add Road")
 
         tk.Label(road_window, text="Allowed Directions").pack()
-        directions = {"Straight": tk.BooleanVar(), "Left": tk.BooleanVar(), "Right": tk.BooleanVar()}
+        directions = {"S": tk.BooleanVar(), "L": tk.BooleanVar(), "R": tk.BooleanVar()}
 
         for direction, var in directions.items():
             tk.Checkbutton(road_window, text=direction, variable=var).pack()
@@ -70,41 +70,41 @@ class TrafficLightConfigurator:
         tk.Checkbutton(road_window, text="Has Pedestrian Light", variable=pedestrian_var).pack()
 
         def save_road():
-            road_data = {
-                "directions": {k: v.get() for k, v in directions.items()},
-                "red_time": int(red_time.get()),
-                "green_time": int(green_time.get()),
-                "pedestrian": pedestrian_var.get()
-            }
-            self.config["roads"].append(road_data)
+            if orientation == Orientation.NORTH:
+                road = RoadNorth(self.canvas, {k: v.get() for k, v in directions.items()}, int(red_time.get()), int(green_time.get()), pedestrian_var.get())
+            elif orientation == Orientation.EAST:
+                road = RoadEast(self.canvas, {k: v.get() for k, v in directions.items()}, int(red_time.get()), int(green_time.get()), pedestrian_var.get())
+            elif orientation == Orientation.SOUTH:
+                road = RoadSouth(self.canvas, {k: v.get() for k, v in directions.items()}, int(red_time.get()), int(green_time.get()), pedestrian_var.get())
+            elif orientation == Orientation.WEST:
+                road = RoadWest(self.canvas, {k: v.get() for k, v in directions.items()}, int(red_time.get()), int(green_time.get()), pedestrian_var.get())
+            self.roads.append(road)
             road_window.destroy()
-            self.draw_road(orientation=orientation, direction=road_data["directions"], pedestrian=road_data["pedestrian"])
+            self.draw_road(road, orientation)
+            self.buttons[orientation].destroy()
+            del self.buttons[orientation]
 
         tk.Button(road_window, text="Save", command=save_road).pack()
 
-
-    def draw_road(self, orientation: Orientation, direction: dict, pedestrian: bool):
-        if orientation == Orientation.NORTH:
-            draw_road_north(self.canvas, 200, 50)
-        elif orientation == Orientation.WEST:
-            draw_road_west(self.canvas, 200, 200)
-        elif orientation == Orientation.SOUTH:
-            draw_road_south(self.canvas, 200, 200)
-        elif orientation == Orientation.EAST:
-            draw_road_east(self.canvas, 50, 200)
-
+    def draw_road(self, road, orientation: Orientation):
+        positions = {
+            Orientation.NORTH: (200, 50),
+            Orientation.WEST: (200, 200),
+            Orientation.SOUTH: (200, 200),
+            Orientation.EAST: (50, 200)
+        }
+        road.draw(*positions[orientation])
 
     def save_config(self):
-        if len(self.config["roads"]) < 3:
+        if len(self.roads) < 3:
             messagebox.showwarning("Minimum Roads", "A crossroad must have at least 3 roads.")
             return
 
         file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json")])
         if file_path:
             with open(file_path, "w") as file:
-                json.dump(self.config, file, indent=4)
+                json.dump([road.__dict__ for road in self.roads], file, indent=4)
             messagebox.showinfo("Saved", "Configuration saved successfully!")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
---
+++ CrossRoadConfig/road.py
@@ -0,0 +1,231 @@
+class Road:
+    def __init__(self, canvas, directions, red_time, green_time, pedestrian):
+        self.canvas = canvas
+        self.directions = directions
+        self.red_time = red_time
+        self.green_time = green_time
+        self.pedestrian = pedestrian
+
+
+    def draw(self, x, y):
+        pass
+
+    def draw_straight_arrow(self, coords):
+        pass
+
+    def draw_left_arrow(self, coords):
+        pass
+
+    def draw_right_arrow(self, coords):
+        pass
+
+
+class RoadNorth(Road):
+    def __init__(self, canvas, directions, red_time, green_time, pedestrian):
+        super().__init__(canvas, directions, red_time, green_time, pedestrian)
+
+    def draw(self, x, y):
+        width, height = 100, 250
+        line_x = x + (width * 3 // 5)
+
+        self.canvas.create_rectangle(x, y, x + width, y + height, fill="gray", outline="gray")
+
+        # Draw black line at 3/5 of the width
+        self.canvas.create_line(line_x, y, line_x, y + height - 102, fill="black", width=2)
+
+        # Draw white stripes
+        for j in [20, 40, 80]:
+            for i in range(y, y + height - 110, 15):
+                self.canvas.create_line(x + j, i, x + j, i + 10, fill="white", width=3)
+
+        # Draw black line in front of the lights
+        self.canvas.create_line(x, y + height - 102, x + width, y + height - 102, fill="black", width=2)
+
+        right_lane_coords = (200, 160)
+        middle_lane_coords = (220, 160)
+        left_lane_coords = (240, 160)
+
+        if self.directions['R']:
+            self.draw_right_arrow(right_lane_coords)
+        else:
+            self.draw_straight_arrow(right_lane_coords)
+
+        if self.directions['L']:
+            self.draw_left_arrow(left_lane_coords)
+        else:
+            self.draw_straight_arrow(left_lane_coords)
+
+        self.draw_straight_arrow(middle_lane_coords)
+
+
+    def draw_straight_arrow(self, coords):
+        x, y = coords
+
+        length_of_arrow = 30
+        middle_of_line = 10
+        offset = 5
+        width = 2
+
+        self.canvas.create_line(x + middle_of_line, y, x + middle_of_line, y + length_of_arrow, fill="white", width=width)
+        self.canvas.create_line(x + offset, y + length_of_arrow - offset, x + middle_of_line, y + length_of_arrow,
+                           fill="white", width=width)
+        self.canvas.create_line(x + middle_of_line + offset, y + length_of_arrow - offset, x + middle_of_line,
+                           y + length_of_arrow, fill="white", width=width)
+
+
+    def draw_left_arrow(self, coords):
+        pass
+
+    def draw_right_arrow(self, coords):
+        pass
+
+
+class RoadSouth(Road):
+    def __init__(self, canvas, directions, red_time, green_time, pedestrian):
+        super().__init__(canvas, directions, red_time, green_time, pedestrian)
+
+    def draw(self, x, y):
+        width, height = 100, 250
+        line_x = x + (width * 2 // 5)
+
+        self.canvas.create_rectangle(x, y, x + width, y + height, fill="gray", outline="gray")
+
+        # Draw black line at 2/5 of the width
+        self.canvas.create_line(line_x, y + 102, line_x, y + height, fill="black", width=2)
+
+        # Draw white stripes
+        for j in [20, 60, 80]:
+            for i in range(y + 110, y + height - 10, 15):
+                self.canvas.create_line(x + j, i, x + j, i + 10, fill="white", width=3)
+
+        # Draw black line in front of the lights
+        self.canvas.create_line(x, y + 102, x + width, y + 102, fill="black", width=2)
+
+        right_lane_coords = (240, 310)
+        middle_lane_coords = (260, 310)
+        left_lane_coords = (280, 310)
+
+        if self.directions['R']:
+            self.draw_right_arrow(right_lane_coords)
+        else:
+            self.draw_straight_arrow(right_lane_coords)
+
+        if self.directions['L']:
+            self.draw_left_arrow(left_lane_coords)
+        else:
+            self.draw_straight_arrow(left_lane_coords)
+
+        self.draw_straight_arrow(middle_lane_coords)
+
+    def draw_straight_arrow(self, coords):
+        x, y = coords
+
+        length_of_arrow = 30
+        middle_of_line = 10
+        offset = 5
+        width = 2
+
+        self.canvas.create_line(x + middle_of_line, y, x + middle_of_line, y + length_of_arrow, fill="white", width=width)
+        self.canvas.create_line(x + offset, y + offset, x + middle_of_line, y, fill="white", width=width)
+        self.canvas.create_line(x + middle_of_line + offset, y + offset, x + middle_of_line, y, fill="white", width=width)
+
+    def draw_left_arrow(self, coords):
+        pass
+
+    def draw_right_arrow(self, coords):
+        pass
+
+
+class RoadEast(Road):
+    def __init__(self, canvas, directions, red_time, green_time, pedestrian):
+        super().__init__(canvas, directions, red_time, green_time, pedestrian)
+
+    def draw(self, x, y):
+        width, height = 250, 100
+        line_y = y + (height * 2 // 5)
+
+        self.canvas.create_rectangle(x, y, x + width, y + height, fill="gray", outline="gray")
+
+        # Draw black line at 2/5 of the height
+        self.canvas.create_line(x, line_y, x + width - 102, line_y, fill="black", width=2)
+
+        # Draw white stripes
+        for j in [20, 60, 80]:
+            for i in range(x, x + width - 110, 15):
+                self.canvas.create_line(i, y + j, i + 10, y + j, fill="white", width=3)
+
+        # Draw black line in front of the lights
+        self.canvas.create_line(x + width - 102, y, x + width - 102, y + height, fill="black", width=2)
+
+        # right_lane_coords = (200, 160)
+        # middle_lane_coords = (220, 160)
+        # left_lane_coords = (240, 160)
+        #
+        # if self.directions['R']:
+        #     self.draw_right_arrow(right_lane_coords)
+        # else:
+        #     self.draw_straight_arrow(right_lane_coords)
+        #
+        # if self.directions['L']:
+        #     self.draw_left_arrow(left_lane_coords)
+        # else:
+        #     self.draw_straight_arrow(left_lane_coords)
+        #
+        # self.draw_straight_arrow(middle_lane_coords)
+
+    def draw_straight_arrow(self, coords):
+        pass
+
+    def draw_left_arrow(self, coords):
+        pass
+
+    def draw_right_arrow(self, coords):
+        pass
+
+
+class RoadWest(Road):
+    def __init__(self, canvas, directions, red_time, green_time, pedestrian):
+        super().__init__(canvas, directions, red_time, green_time, pedestrian)
+
+    def draw(self, x, y):
+        width, height = 250, 100
+        line_y = y + (height * 3 // 5)
+
+        self.canvas.create_rectangle(x, y, x + width, y + height, fill="gray", outline="gray")
+
+        # Draw black line at 3/5 of the height
+        self.canvas.create_line(x + 102, line_y, x + width, line_y, fill="black", width=2)
+
+        # Draw white stripes
+        for j in [20, 40, 80]:
+            for i in range(x + 110, x + width - 10, 15):
+                self.canvas.create_line(i, y + j, i + 10, y + j, fill="white", width=3)
+
+        # Draw black line in front of the lights
+        self.canvas.create_line(x + 102, y, x + 102, y + height, fill="black", width=2)
+
+        # right_lane_coords = (200, 160)
+        # middle_lane_coords = (220, 160)
+        # left_lane_coords = (240, 160)
+        #
+        # if self.directions['R']:
+        #     self.draw_right_arrow(right_lane_coords)
+        # else:
+        #     self.draw_straight_arrow(right_lane_coords)
+        #
+        # if self.directions['L']:
+        #     self.draw_left_arrow(left_lane_coords)
+        # else:
+        #     self.draw_straight_arrow(left_lane_coords)
+        #
+        # self.draw_straight_arrow(middle_lane_coords)
+
+
+    def draw_straight_arrow(self, coords):
+        pass
+
+    def draw_left_arrow(self, coords):
+        pass
+
+    def draw_right_arrow(self, coords):
+        pass
