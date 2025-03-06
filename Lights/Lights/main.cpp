5cf879ea519b92aa882b6f6fda24392adefa1055
Daniel Šoltýs <danielsytlos@gmail.com>
2025-03-06 18:22:23 +0100

    Added working EEPROM saving of crossroad config with comments

--- Lights/Lights/main.cpp
+++ Lights/Lights/main.cpp
@@ -9,31 +9,12 @@
 #include <avr/io.h>
 extern "C" void USART_init(unsigned int ubrr, unsigned int data_bits, unsigned int stop_bits);
 extern "C" void USART_ReadConfig(void);
-extern "C" uint8_t EEPROM_ReadByte(uint8_t address);
-extern "C" void USART_Transmit(uint8_t data);
 
-
-void USART_Print(const char* str) {
-	while (*str) {
-		USART_Transmit(*str++);
-	}
-}
-
-void USART_PrintHex(uint8_t value) {
-	const char hex_chars[] = "0123456789ABCDEF";
-	USART_Transmit(hex_chars[(value >> 4) & 0x0F]);  // High nibble
-	USART_Transmit(hex_chars[value & 0x0F]);         // Low nibble
-}
 
 int main(void) {
 	USART_init(MYUBRR, DATA_BITS, STOP_BITS);
 	
 	while(1){
 		USART_ReadConfig();
-		
-		uint8_t firstByte = EEPROM_ReadByte(0);
-		
-		uint8_t secondByte = EEPROM_ReadByte(1);
-		
 	}
 }
