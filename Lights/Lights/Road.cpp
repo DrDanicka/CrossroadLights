a1b0440f882e5c4d2ff304c91cf8482b321b703c
Daniel Šoltýs <danielsytlos@gmail.com>
2025-03-11 08:45:17 +0100

    Added Road light control

--- Lights/Lights/Road.cpp
+++ Lights/Lights/Road.cpp
@@ -1,5 +1,13 @@
 #include "Road.h"
 
+uint8_t right_pins[3] = {0, 1, 2};
+uint8_t left_pins[3] = {3, 4, 5};
+uint8_t straight_pins[3] = {3, 4, 5};
+uint8_t expanderAddresses[4] = {NORTH_ADDRESS, EAST_ADDRESS, SOUTH_ADDRESS, WEST_ADDRESS};
+
+Road::Road() : orientation(0), straight(false), left(false), right(false),
+green_light(0), pedestrians(false), exists(false) {}
+
 Road::Road(uint8_t byte1, uint8_t byte2){
 	/*
 	O = Orientation
@@ -15,9 +23,171 @@ Road::Road(uint8_t byte1, uint8_t byte2){
 	byte2:
 	| Gt | Gt | Gt | Gt | Gt | Gt | Gt | P | 
 	*/
+	orientation = (byte1 >> 6) & 0x03;
 	straight = (byte1 & (1 << 5));
 	left = (byte1 & (1 << 4));
 	right = (byte1 & (1 << 3));
 	green_light = ((byte1 & 0x07) << 7) | ((byte2 & 0xFE) >> 1);
+	green_light += 3; // Because of orange/red switching
 	pedestrians = (byte2 & (1 << 0));
+	
+	expanderAddress = expanderAddresses[orientation];
+	
+	exists = !(!straight && !left && !right);
+}
+
+// Helper function that sets state of one light in direction at light index
+void Road::turnOnOneLight(Directions direction, uint8_t light_index, bool state){
+	if (direction == Directions::STRAIGHT && straight){
+		for(uint8_t i = 0; i < 3; i++){
+			if (i == light_index){
+				setPinState(expanderAddress, REG_OUTPUT_PORTA, straight_pins[i], state);
+			}else{
+				setPinState(expanderAddress, REG_OUTPUT_PORTA, straight_pins[i], !state);
+			}
+		}
+	}else if (direction == Directions::LEFT && left){
+		for(uint8_t i = 0; i < 3; i++){
+			if (i == light_index){
+				setPinState(expanderAddress, REG_OUTPUT_PORTB, left_pins[i], state);
+			}else{
+				setPinState(expanderAddress, REG_OUTPUT_PORTB, left_pins[i], !state);
+			}
+		}
+	}else if (direction == Directions::RIGHT && right){
+		for(uint8_t i = 0; i < 3; i++){
+			if (i == light_index){
+				setPinState(expanderAddress, REG_OUTPUT_PORTA, right_pins[i], state);
+			}else{
+				setPinState(expanderAddress, REG_OUTPUT_PORTA, right_pins[i], !state);
+			}
+		}
+	}
+}
+
+// Turn on green in direction
+void Road::green(Directions direction){
+	uint8_t green_index = 2;
+	
+	turnOnOneLight(direction, green_index, true);
+}
+
+// Turn on orange in direction
+void Road::orange(Directions direction){
+	uint8_t orange_index = 1;
+	
+	turnOnOneLight(direction, orange_index, true);
+}
+
+// Turn on red in direction
+void Road::red(Directions direction){
+	uint8_t red_index = 0;
+	
+	turnOnOneLight(direction, red_index, true);
+}
+
+// Turn on red and orange at the same time in direction
+void Road::redOrange(Directions direction){
+	uint8_t green_index = 2;
+	
+	// Turns off only green light and tuns on red and orange
+	turnOnOneLight(direction, green_index, false);
+}
+
+void Road::greenPedestrians(){
+	// Green is connected to port 1
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 1, true);
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 0, false);
+}
+
+void Road::redPedestrians(){
+	// Green is connected to port 2
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 0, true);
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 1, false);
+}
+
+// Turns off car traffic light in the direction
+void Road::turnOff(Directions direction){
+	if (direction == Directions::STRAIGHT){
+		for(uint8_t i = 0; i < 3; i++){
+			setPinState(expanderAddress, REG_OUTPUT_PORTA, straight_pins[i], 0);
+		}
+	}else if (direction == Directions::LEFT){
+		for(uint8_t i = 0; i < 3; i++){
+			setPinState(expanderAddress, REG_OUTPUT_PORTB, left_pins[i], 0);
+		}
+	}else{
+		for(uint8_t i = 0; i < 3; i++){
+			setPinState(expanderAddress, REG_OUTPUT_PORTA, right_pins[i], 0);
+		}
+	}
+}
+
+// Turns off pedestrian lights
+void Road::turnOffPedestrians(){
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 1, false);
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, 0, false);
+}
+
+// Sets all existing lights to red
+void Road::allRed(){
+	if (straight){
+		red(Directions::STRAIGHT);	
+	}else{
+		turnOff(Directions::STRAIGHT);
+	}
+	
+	if (left){
+		red(Directions::LEFT);
+	}else{
+		turnOff(Directions::LEFT);
+	}
+	
+	if (right){
+		red(Directions::RIGHT);	
+	} else{
+		turnOff(Directions::RIGHT);
+	}
+	
+	if (pedestrians){
+		redPedestrians();	
+	}else{
+		turnOffPedestrians();
+	}
+}
+
+// Sets all lights to orange
+void Road::allOrange(){
+	uint8_t orange_pin = 1;
+	
+	setPinState(expanderAddress, REG_OUTPUT_PORTA, straight_pins[orange_pin], 1);
+	setPinState(expanderAddress, REG_OUTPUT_PORTB, left_pins[orange_pin], 1);
+	setPinState(expanderAddress, REG_OUTPUT_PORTA, right_pins[orange_pin], 1);
+}
+
+bool Road::buttonClicked(){
+	// Read the current state of PB2 (pin of button) at address 0x01
+	uint8_t address = 0x01;
+	bool currentButtonState = readPinState(expanderAddress, address, 2);
+
+	
+	// Check if the button state has changed
+	if (currentButtonState != lastButtonState) {
+        lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > DEBOUNCE_TIME) {
+        if (currentButtonState != buttonPressed) {
+            buttonPressed = currentButtonState;
+
+            if (!buttonPressed) {
+                return true; // Button was clicked
+            }
+        }
+    }
+
+    // Update the last button state
+    lastButtonState = currentButtonState;
+	
+	return false;
 }
--- Lights/Lights/Road.h
+++ Lights/Lights/Road.h
@@ -1,7 +1,17 @@
 #ifndef ROAD_H_
 #define ROAD_H_
 
-#include <stdint.h>
+#include <avr/io.h>
+#include "twi.h"
+#include "MyTimer.h"
+
+#define DEBOUNCE_TIME 50
+
+enum Directions{
+	STRAIGHT,
+	LEFT,
+	RIGHT	
+};
 
 class Road{
 public:
@@ -12,7 +22,31 @@ public:
 	bool pedestrians;		// Has zebra for pedestrians
 	bool exists;			// Flag that based of the directions finds out if road exists
 	
+	Road();
+	
 	Road(uint8_t byte1, uint8_t byte2);
+	
+	// Light functions
+	void green(Directions direction);
+	void red(Directions direction);
+	void orange(Directions direction);
+	void redOrange(Directions direction);
+	void greenPedestrians();
+	void redPedestrians();
+	void allRed();
+	void turnOff(Directions direction);
+	void turnOffPedestrians();
+	void allOrange();
+	
+	bool buttonClicked();	// Check if pedestrian light was clicked
+private:
+	uint8_t orientation;
+	uint8_t expanderAddress = 0x00;
+	bool lastButtonState = true;
+	bool buttonPressed = true;
+	uint32_t lastDebounceTime = 0;
+	
+	void turnOnOneLight(Directions direction, uint8_t light_index, bool state);
 };
 
 
